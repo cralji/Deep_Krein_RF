70c242e640a913f4e6e68f45e316dce12c3bbe76
craljimenez <craljimenez@utp.edu.co>
2022-10-20 09:06:24 -0500

    add new results from a bayesian optimize

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-from krein_functions import Krein_mapping #Deep_Krein_RF.krein_functions import Krein_mapping
+from Deep_Krein_RF.krein_functions import Krein_mapping
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.regularizers import OrthogonalRegularizer
