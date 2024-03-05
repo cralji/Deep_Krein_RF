7ee4c3ded6770750e00cec20b9a69ef6187c4cc4
Cristian J <craljimenez@utp.edu.co>
2024-03-05 15:49:09 -0500

    add Genelarized Dice Loss and overall

--- losses.py
+++ losses.py
@@ -1,9 +1,9 @@
 #%% Libraries
-from tensorflow.keras.losses import Loss
+from tensorflow.keras.losses import Loss,CategoricalCrossentropy
 from tensorflow.keras import backend as BK
 import tensorflow.linalg as lia
 import tensorflow.math as ma
-from tensorflow import clip_by_value
+from tensorflow import clip_by_value,reshape
 #%% Losses
 # Dual Focal Loss Segmentation
 
@@ -32,5 +32,47 @@ class dual_focal_loss(Loss):
         loss2 = ma.reduce_mean(loss1,axis=[1,2])
         return ma.reduce_mean(loss2)
 
+class GDL(Loss):
+    def __init__(self,
+                 epsilon,
+                 **kwargs):
+        super(GDL,self).__init__(**kwargs)
+        self.epsilon = epsilon
+    
+    def call(self,y_true,y_pred):
+        """
+        Generalized Dice loss (GDL) [26]
+        """
+        y_true_f = reshape(y_true, [-1])
+        y_pred_f = reshape(y_pred, [-1])
+        sum_p = ma.reduce_sum(y_pred_f)
+        sum_r = ma.reduce_sum(y_true_f)
+        sum_pr = ma.reduce_sum(y_pred_f * y_true_f)
+        weights = 1.0 / (ma.square(sum_r) + self.epsilon)
+
+        generalized_dice_loss = 1 - 2 * ( sum_pr * weights) / (sum_r + sum_p + self.epsilon)
+        
+        return generalized_dice_loss
+
+class overall_loss(Loss):
+    def __init__(self,
+                ld = 1.25,
+                epsilon = 1e-4,
+                **kwargs):
+        super(overall_loss,self).__init__(**kwargs)
+        self.ld = ld
+        self.epsilon = epsilon
+        self.CE = CategoricalCrossentropy()
+        self.GDL = GDL(epsilon=epsilon)
+        
+
+    def call(self,y_true,y_pred):
+        return self.GDL(y_true,y_pred) + self.ld*self.CE(y_true,y_pred)
+
+
+        
+
         
         
+
+# %%
